1e5ccff0c9c14d617c540a9de14923d410d56b6d
kandika-Rohan <rohankandika05@gmail.com>
2025-03-12 14:20:55 +0530

    Update server.js
    updated the server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
-// const port =process.env.PORT || 5000;
-const port=5000;
+const port =process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
